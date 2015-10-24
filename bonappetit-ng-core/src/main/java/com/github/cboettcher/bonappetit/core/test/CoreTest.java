47bdbeda89186bb93be3d87d046bdfcd698a55a5
Claudius Boettcher <claudius.boettcher@gmail.com>
2015-10-24 21:51:23 +0200

    Adds property files, jax-rs-server -> core dependency, some code to test the dependency resolution

---
+++ bonappetit-ng-core/src/main/java/com/github/cboettcher/bonappetit/core/test/CoreTest.java
@@ -0,0 +1,10 @@
+package com.github.cboettcher.bonappetit.core.test;
+
+/**
+ *
+ */
+public class CoreTest {
+    public static String sayHello(String name) {
+        return "Hello " + name;
+    }
+}
