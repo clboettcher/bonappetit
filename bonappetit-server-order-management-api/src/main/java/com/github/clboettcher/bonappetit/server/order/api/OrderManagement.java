632ee53d37f25ccac3755eb4fa54e9e091ee4fb7
Claudius Böttcher <c.boettcher@muc.qaware.local>
2016-11-08 11:29:14 +0100

    Added order management API interface.

---
+++ bonappetit-server-order-management-api/src/main/java/com/github/clboettcher/bonappetit/server/order/api/OrderManagement.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2016 Claudius Boettcher (pos.bonappetit@gmail.com)
+ *
+ * This file is part of BonAppetit. BonAppetit is an Android based
+ * Point-of-Sale client-server application for small restaurants.
+ *
+ * BonAppetit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BonAppetit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BonAppetit.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.clboettcher.bonappetit.server.order.api;
+
+
+import com.github.clboettcher.bonappetit.server.order.api.dto.ItemOrderDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Collection;
+
+@Path(OrderManagement.ROOT_PATH)
+@Api(value = OrderManagement.ROOT_PATH)
+public interface OrderManagement {
+
+    String ROOT_PATH = "/orders";
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Creates the given orders in the database. " +
+            "After the orders have been saved they are printed. If printing" +
+            "fails, the orders are not deleted from the database.")
+    Response createOrders(Collection<ItemOrderDto> orders);
+}
