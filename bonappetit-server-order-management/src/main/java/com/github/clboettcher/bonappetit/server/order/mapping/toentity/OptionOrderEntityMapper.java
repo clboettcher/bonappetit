72f43c59d5ac43e9a7c22127a76cac0e70ed92d1
Claudius Böttcher <c.boettcher@muc.qaware.local>
2016-11-26 15:29:31 +0100

    Implemented mapping since we need to query the db

--- bonappetit-server-order-management/src/main/java/com/github/clboettcher/bonappetit/server/order/mapping/toentity/OptionOrderEntityMapper.java
+++ bonappetit-server-order-management/src/main/java/com/github/clboettcher/bonappetit/server/order/mapping/toentity/OptionOrderEntityMapper.java
@@ -2,7 +2,9 @@ package com.github.clboettcher.bonappetit.server.order.mapping.toentity;
 
 
 import com.github.clboettcher.bonappetit.server.menu.impl.dao.OptionDao;
+import com.github.clboettcher.bonappetit.server.menu.impl.entity.menu.AbstractOptionEntity;
 import com.github.clboettcher.bonappetit.server.menu.impl.entity.menu.CheckboxOptionEntity;
+import com.github.clboettcher.bonappetit.server.menu.impl.entity.menu.ValueOptionEntity;
 import com.github.clboettcher.bonappetit.server.order.api.dto.write.CheckboxOptionOrderCreationDto;
 import com.github.clboettcher.bonappetit.server.order.api.dto.write.OptionOrderCreationDto;
 import com.github.clboettcher.bonappetit.server.order.api.dto.write.RadioOptionOrderCreationDto;
@@ -15,7 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring", uses = {RadioOptionOrderEntityMapper.class})
+@Mapper(componentModel = "spring")
 public abstract class OptionOrderEntityMapper {
 
     @Autowired
@@ -24,16 +26,8 @@ public abstract class OptionOrderEntityMapper {
     @Autowired
     private OptionDao optionDao;
 
-    /**
-     * @param options The dto to map.
-     * @return The mapping result.
-     */
     public abstract List<AbstractOptionOrderEntity> mapToOptionDtos(List<OptionOrderCreationDto> options);
 
-    /**
-     * @param option The {@link OptionOrderCreationDto} to map.
-     * @return The mapping result.
-     */
     public AbstractOptionOrderEntity mapToOptionOrderEntity(OptionOrderCreationDto option) {
         if (option instanceof RadioOptionOrderCreationDto) {
             return radioOptionOrderEntityMapper.mapToRadioOptionDto((RadioOptionOrderCreationDto) option);
@@ -46,27 +40,34 @@ public abstract class OptionOrderEntityMapper {
         }
     }
 
-    /**
-     * @param option The dto to map.
-     * @return The mapping result.
-     */
     public CheckboxOptionOrderEntity mapToCheckboxOptionDto(CheckboxOptionOrderCreationDto option) {
         if (option == null) {
             return null;
         }
 
         CheckboxOptionOrderEntity result = new CheckboxOptionOrderEntity();
+        // Existence and correct type of referenced option have been validated before.
         CheckboxOptionEntity checkboxOption = (CheckboxOptionEntity) optionDao.getOptionById(option.getOptionId());
 
         result.setCheckboxOption(checkboxOption);
         result.setChecked(option.getChecked());
 
         return result;
-    };
+    }
 
-    /**
-     * @param valueOption The {@link ValueOptionOrderCreationDto} to map.
-     * @return The mapping result.
-     */
-    public abstract ValueOptionOrderEntity mapToValueOptionDto(ValueOptionOrderCreationDto valueOption);
+    public ValueOptionOrderEntity mapToValueOptionDto(ValueOptionOrderCreationDto valueOption) {
+        if (valueOption == null) {
+            return null;
+        }
+
+        ValueOptionOrderEntity result = new ValueOptionOrderEntity();
+
+        result.setValue(valueOption.getValue());
+
+        // Existence and correct type of referenced option have been validated before.
+        AbstractOptionEntity optionEntity = optionDao.getOptionById(valueOption.getOptionId());
+        result.setValueOption((ValueOptionEntity) optionEntity);
+
+        return result;
+    }
 }
--- bonappetit-server-order-management/src/main/java/com/github/clboettcher/bonappetit/server/order/mapping/toentity/RadioOptionOrderEntityMapper.java
+++ bonappetit-server-order-management/src/main/java/com/github/clboettcher/bonappetit/server/order/mapping/toentity/RadioOptionOrderEntityMapper.java
@@ -1,15 +1,27 @@
 package com.github.clboettcher.bonappetit.server.order.mapping.toentity;
 
+import com.github.clboettcher.bonappetit.server.menu.impl.dao.RadioItemDao;
 import com.github.clboettcher.bonappetit.server.order.api.dto.write.RadioOptionOrderCreationDto;
 import com.github.clboettcher.bonappetit.server.order.entity.RadioOptionOrderEntity;
 import org.mapstruct.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
-public interface RadioOptionOrderEntityMapper {
+public abstract class RadioOptionOrderEntityMapper {
 
-    /**
-     * @param radioOption The {@link RadioOptionOrderCreationDto} to map.
-     * @return The mapping result.
-     */
-    RadioOptionOrderEntity mapToRadioOptionDto(RadioOptionOrderCreationDto radioOption);
+    @Autowired
+    private RadioItemDao radioItemDao;
+
+    RadioOptionOrderEntity mapToRadioOptionDto(RadioOptionOrderCreationDto radioOption) {
+        if (radioOption == null) {
+            return null;
+        }
+
+        RadioOptionOrderEntity result = new RadioOptionOrderEntity();
+
+        // Existence of referenced radio item has been validate before.
+        result.setSelectedRadioItem(radioItemDao.getRadioItemById(radioOption.getSelectedRadioItemId()));
+
+        return result;
+    }
 }
