0cb25f9b1287c0b4440813a30369525b42541494
Claudius Böttcher <c.boettcher@muc.qaware.local>
2016-12-10 11:10:32 +0100

    Added spring security and basic conf class

---
+++ bonappetit-server-app/src/main/java/com/github/clboettcher/bonappetit/server/SecurityConfig.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2016 Claudius Boettcher (pos.bonappetit@gmail.com)
+ *
+ * This file is part of BonAppetit. BonAppetit is an Android based
+ * Point-of-Sale client-server application for small restaurants.
+ *
+ * BonAppetit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BonAppetit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BonAppetit.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.clboettcher.bonappetit.server;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Spring security config for managing authentication and authorization for
+ * API endpoints.
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                // Spring Security should completely ignore URLs starting with /resources/
+                .antMatchers("/resources/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+                .authorizeRequests()
+                    .anyRequest().hasRole("USER")
+                .and()
+                    .httpBasic();
+        // @formatter:on
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                // enable in memory based authentication with a user named "user" and "admin"
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER").and()
+                .withUser("admin").password("password").roles("USER", "ADMIN");
+    }
+}
