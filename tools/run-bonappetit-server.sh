7fdf41f94451f1ce4299a195e1ca69062c2d4c2c
Claudius Boettcher <claudius.boettcher@gmail.com>
2017-01-08 18:21:27 +0100

    #73 Added --spring.config.location param to server start script tool.

--- tools/run-bonappetit-server.sh
+++ tools/run-bonappetit-server.sh
@@ -3,12 +3,12 @@
 
 # Comma separated list of spring boot profiles to activate.
 PROFILES=$1
-VERSION=0.0.1-SNAPSHOT
+VERSION=0.1.0-ALPHA
 BASE_NAME=bonappetit-server
 JAR_LOCATION=.
 BONAPPETIT_BASE_DIR="M:\BONAPPETIT_BASE"
+BONAPPETIT_PROPERTIES_LOCATION="${BONAPPETIT_BASE_DIR}\content\config\bonappetit.properties"
 
-
-java -jar -DBONAPPETIT_BASE=${BONAPPETIT_BASE_DIR} -Dspring.profiles.active=${PROFILES} ${JAR_LOCATION}/${BASE_NAME}-${VERSION}.jar
+java -jar -DBONAPPETIT_BASE=${BONAPPETIT_BASE_DIR} -Dspring.profiles.active=${PROFILES} ${JAR_LOCATION}/${BASE_NAME}-${VERSION}.jar --spring.config.location ${BONAPPETIT_PROPERTIES_LOCATION}
 
 exit 0
