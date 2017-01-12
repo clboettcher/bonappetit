20d2eca5855289248a2664ffd828cd7a9f65d630
Claudius Boettcher <claudius.boettcher@gmail.com>
2017-01-12 20:36:24 +0100

    #13 Removed profile param and hardcoded it in the script, corrected server version.

--- tools/run-bonappetit-server.sh
+++ tools/run-bonappetit-server.sh
@@ -2,12 +2,12 @@
 # Runs the bonappetit server.
 
 # Comma separated list of spring boot profiles to activate.
-PROFILES=$1
-VERSION=0.1.0-ALPHA
+PROFILES=PROD
+VERSION=0.2.0-ALPHA
 BASE_NAME=bonappetit-server
 JAR_LOCATION=.
 BONAPPETIT_BASE_DIR="M:\BONAPPETIT_BASE"
-BONAPPETIT_PROPERTIES_LOCATION="${BONAPPETIT_BASE_DIR}\content\config\bonappetit.properties"
+BONAPPETIT_PROPERTIES_LOCATION="${BONAPPETIT_BASE_DIR}\config\bonappetit.properties"
 
 java -jar -DBONAPPETIT_BASE=${BONAPPETIT_BASE_DIR} -Dspring.profiles.active=${PROFILES} ${JAR_LOCATION}/${BASE_NAME}-${VERSION}.jar --spring.config.location ${BONAPPETIT_PROPERTIES_LOCATION}
 
