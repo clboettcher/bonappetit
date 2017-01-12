11e63157a635474c0a0d33a1e6a5ccf4362cbe8a
Claudius Boettcher <claudius.boettcher@gmail.com>
2017-01-12 20:58:55 +0100

    #13 Added alias file that can be sourced in bashrc providing useful commands.

---
+++ tools/bash_aliases.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# documentation
+alias doc="/m/codebase/bonappetit-server/gradlew publishGhPages"
+
+# git
+alias g="git"
+alias gs="git status"
+alias gf="git fetch"
+alias gpl="git pull"
+alias gp="git push"
+alias gc="git commit"
+alias gca="git commit --amend"
+alias gm="git merge"
+alias gco="git checkout"
